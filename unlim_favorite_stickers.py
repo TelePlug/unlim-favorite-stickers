a87fe3d071ec556585da83632abdcf5c7a084073
Даниил Шевкунов <danila-763@yandex.ru>
2026-08-22 11:19:28 +0300

    chore(release): 2.2.0
    Минор, а не патч: добавилась функциональность - бекап избранного.
    В авторы добавлен @teleplugit.

--- unlim_favorite_stickers.py
+++ unlim_favorite_stickers.py
@@ -16,8 +16,8 @@ from ui.settings import Divider, Header, Text
 __id__ = "favstickers"
 __name__ = "Unlim favorite stickers"
 __description__ = "Remove limits on adding stickers to favorites, with backup"
-__author__ = "@DaShMore"
-__version__ = "2.1.0"
+__author__ = "@DaShMore & @teleplugit"
+__version__ = "2.2.0"
 __icon__ = "plugins_covers/0"
 __min_version__ = "11.12.0"
 
